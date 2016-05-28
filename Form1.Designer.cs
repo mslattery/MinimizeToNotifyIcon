21ae74a6959b0ec165b677198cd692806491ab92
Michael Slattery <mslattery@sncsoftware.com>
2016-05-28 07:47:48 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace MinimizeToNotifyIcon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Minimize To Notify Icon";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(63, 93);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(323, 57);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Really Close Form";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 320);
+            this.Controls.Add(this.btnClose);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MinimizeToNotifyIcon
+{
+    public partial class Form1 : Form
+    {
+        private const int NOTIFY_ICON_SHOW_TIME = 300;
+        private const string NOTIFY_ICON_TITLE = "Minimize to Notify Icon";
+
+        // Used to control visibility other than the first startups
+        private bool HideForm = true;
+
+        // Used to handle form startup
+        private bool HideOnStartup = true;
+
+        // Used to allow for real form closing
+        private bool ReallyClose = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (HideOnStartup) { HideOnStartup = false; } // Reverses the startup case
+            HideForm = false;
+            this.Show();
+            WindowState = FormWindowState.Normal; // Guarentees the form does not just show to the taskbar, but has focus
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = true; // Needed to ensure after we hide the form (startup) that a notify Icon appears
+            notifyIcon1.ShowBalloonTip(NOTIFY_ICON_SHOW_TIME, NOTIFY_ICON_TITLE, "Starting Minimized to Tray", ToolTipIcon.Info);
+        }
+
+        protected override void OnVisibleChanged(EventArgs e)
+        {
+            if (HideOnStartup) { this.Visible = false; } // Handle Startup
+
+            if (!HideForm) { this.Visible = true; } // Handle Visibilty beyond Startup
+            base.OnVisibleChanged(e);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(NOTIFY_ICON_SHOW_TIME, NOTIFY_ICON_TITLE, "I was minimized", ToolTipIcon.Info);
+            }
+            else if (WindowState == FormWindowState.Normal)
+            {
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!ReallyClose) // Really close if we push the Really close button
+            {
+                HideForm = true;  // Set so visibiity is hidden
+                WindowState = FormWindowState.Minimized;
+                e.Cancel = true;
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            ReallyClose = true;
+            Close();
+        }
+    }
+}
